209894b665cb6308ff53c8501ab8f5b5b6625b8a
Jaka Tertinek <tertinek.jaka@gmail.com>
2019-06-06 14:47:09 +0200

    Alert window on edit schedule

--- WhenDidIDoIt/android/app/src/main/java/com/whendididoit/MainApplication.java
+++ WhenDidIDoIt/android/app/src/main/java/com/whendididoit/MainApplication.java
@@ -3,6 +3,10 @@ package com.whendididoit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -24,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new MapsPackage(),
+            new LinearGradientPackage(),
+            new AsyncStoragePackage(),
             new VectorIconsPackage(),
           new RNGestureHandlerPackage()
       );
